5c53e5a47b3fddf8bb85828c1911638d7421c31d
kaiseroman <romantrud6162@gmail.com>
2024-01-25 17:11:42 +0300

    Добавлен класс точки и метод нахождения расстояния между двумя точками.

--- invertTDOA/invertTDOA.cpp
+++ invertTDOA/invertTDOA.cpp
@@ -3,7 +3,21 @@
 
 using namespace std;
 
+class point
+{
+public:
+	double x;
+	double y;
+
+	static double dist(point p1, point p2) {
+		double d;
+		d = sqrt(pow((p1.x - p2.x), 2) + pow((p1.y - p2.y), 2));
+		return d;
+	}
+};
+
 int main()
 {
+	system("pause");
 	return 0;
 }
