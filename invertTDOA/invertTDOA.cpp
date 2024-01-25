2877c72909a5c11f7eabd9a94c8821d9cb0c5958
kaiseroman <romantrud6162@gmail.com>
2024-01-25 17:13:54 +0300

    Инициализировал точки и разности ходя для проверки.

--- invertTDOA/invertTDOA.cpp
+++ invertTDOA/invertTDOA.cpp
@@ -18,6 +18,24 @@ public:
 
 int main()
 {
+	point pA, pB, pC, pD, pE, pF;
+
+	//Значения точек для проверки:
+	pA.x = 0; //25
+	pA.y = 0; //-15
+	pB.x = 0; //20
+	pB.y = 0; //20
+	pC.x = 0; //-32
+	pC.y = 0; //12
+	pD.x = -36;
+	pD.y = 30;
+	pE.x = 18;
+	pE.y = 14;
+	pF.x = -10;
+	pF.y = -30;
+
+	double D_AB = 18.916, D_BC = 38.447, D_AC = 57.363, E_AB = 23.508, E_BC = -43.715, E_AC = -20.207, F_AB = -20.231, F_BC = 10.897, F_AC = -9.334;
+
 	system("pause");
 	return 0;
 }
